08f59be32644f72669ce22b02c770ae3411b61e4
austinshin <shinaustin@gmail.com>
2018-10-15 16:44:51 -0700

    init

---
+++ src/components/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>My Todo List!</h1>
+    </br>
+    <ul>
+      <li v-for="item in listItems">{{ item }} </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  data () {
+    return {
+      listItems: ['buy food', 'play games', 'sleep']
+    }
+  }
+}
+
+</script>
+
+<style lang='stylus'>
+
+</style>
